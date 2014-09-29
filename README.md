310d3ce8a206c188c9010a4aef944922c60f0e62
Sergio Lopez <wapankh@tardis.lan>
2014-09-29 14:26:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# CIE Calculator
+
+[![Build Status](https://travis-ci.org/slopezm-adquira/cie-calculator.svg)](https://travis-ci.org/slopezm-adquira/cie-calculator)
+
+---
+
+> Easily generate CIE References for BBVA Bancomer
+
+## Minimal Usage
+
+```
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+  <script src="../dist/ciecalculator.js" type="text/javascript"></script>
+</head>
+<body>
+  <script type="text/javascript" charset="utf-8">
+    console.log(CIECalculator({alg: '35', reference: '12346342'}));
+  </script>
+</body>
+</html>
+
+```
+
+## Supported Algorithms
+
+* 00
+* 02
+* 03
+* 04
+* 06
+* 10
+* 35
+* 36
+* 37
+
+
+## Changelog
+
+`0.0.1` : Initial release
+
+## Contribute
+
+
+### Dev
+
+```
+npm run watch
+```
+
+### Test
+
+```
+npm test
+```
+
+### Build
+
+```
+npm run build-debug
+npm run build
+```
+
+
+
---
+++ test/lib/strategies/cie_strategy.js
@@ -0,0 +1,337 @@
+'use strict';
+
+var client = require('../../../main.js');
+var CIECalculator = require('../../../lib/strategies/cie_calculator.js').CIECalculator;
+var Alg00 = require('../../../lib/strategies/alg00.js').Alg00;
+var Alg02 = require('../../../lib/strategies/alg02.js').Alg02;
+var Alg03 = require('../../../lib/strategies/alg03.js').Alg03;
+var Alg04 = require('../../../lib/strategies/alg04.js').Alg04;
+var Alg06 = require('../../../lib/strategies/alg06.js').Alg06;
+var Alg10 = require('../../../lib/strategies/alg10.js').Alg10;
+var Alg35 = require('../../../lib/strategies/alg35.js').Alg35;
+var Alg36 = require('../../../lib/strategies/alg36.js').Alg36;
+var Alg37 = require('../../../lib/strategies/alg37.js').Alg37;
+var calculator;
+
+
+describe('main', function() { 
+
+  it('should calculate the check digit', function(done) {
+    client({alg: '00', reference: '18000359700002387'}).should.eql('180003597000023871');
+    done();
+  });
+});
+
+
+describe('alg 00', function() {
+
+  beforeEach(function() {
+    calculator = new CIECalculator();
+    calculator.setStrategy(new Alg00());
+  });
+
+  it('should calculate the check digit', function(done) {
+    calculator.execute({reference: '18000359700002387'}).should.eql('180003597000023871');
+    calculator.execute({reference: '123456789'}).should.eql('1234567897');
+    calculator.execute({reference: '0000001'}).should.eql('00000018');
+    calculator.execute({reference: '999999999999999999'}).should.eql('9999999999999999998');
+    calculator.execute({reference: '537639480322267'}).should.eql('5376394803222670');
+    done();
+  });
+
+  it('should throw an error if length is longer than 19',function(done) {
+    (function() {
+      calculator.execute({reference: '12345678901234567890'});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+
+  it('should throw an error if reference is zero length',function(done) {
+    (function() {
+      calculator.execute({reference: ''});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+});
+
+describe('alg 02', function() {
+
+  beforeEach(function() {
+    calculator = new CIECalculator();
+    calculator.setStrategy(new Alg02());
+  });
+
+  it('should calculate the check digit', function(done) {
+    calculator.execute({reference: '96060576JMG02158'}).should.eql('96060576JMG021582');
+    calculator.execute({reference: '14082076JM'}).should.eql('14082076JM3');
+    calculator.execute({reference: '00011772XXYA21'}).should.eql('00011772XXYA219');
+    done();
+  });
+
+  it('should accept lower case', function(done) {
+    calculator.execute({reference: '00011772xxya21'}).should.eql('00011772XXYA219');
+    done();
+  });
+
+  it('should throw an error if length is longer than 19',function(done) {
+    (function() {
+      calculator.execute({reference: '12345678901234567890'});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+
+  it('should throw an error if reference length is less than 6',function(done) {
+    (function() {
+      calculator.execute({reference: '96060'});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+});
+
+describe('alg 03', function() {
+
+  beforeEach(function() {
+    calculator = new CIECalculator();
+    calculator.setStrategy(new Alg03());
+  });
+
+  it('should calculate the check digit', function(done) {
+    calculator.execute({reference: '926314223'}).should.eql('9263142233');
+    calculator.execute({reference: '18000359700002387'}).should.eql('180003597000023873');
+    calculator.execute({reference: '123456789'}).should.eql('1234567893');
+    calculator.execute({reference: '001'}).should.eql('0015');
+    calculator.execute({reference: '999999999999999999'}).should.eql('9999999999999999992');
+    done();
+  });
+
+  it('should throw an error if length is longer than 19',function(done) {
+    (function() {
+      calculator.execute({reference: '12345678901234567890'});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+
+  it('should throw an error if reference is zero length',function(done) {
+    (function() {
+      calculator.execute({reference: ''});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+});
+
+describe('alg 04', function() {
+
+  beforeEach(function() {
+    calculator = new CIECalculator();
+    calculator.setStrategy(new Alg04());
+  });
+
+  it('should calculate the check digit', function(done) {
+    calculator.execute({reference: '07680500000300070'}).should.eql('0768050000030007014');
+    calculator.execute({reference: '07680500000300071'}).should.eql('0768050000030007114');
+    calculator.execute({reference: '1'}).should.eql('140');
+    calculator.execute({reference: '198231223488'}).should.eql('19823122348856');
+    done();
+  });
+
+  it('should throw an error if length is longer than 18',function(done) {
+    (function() {
+      calculator.execute({reference: '1234567890123456789'});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+
+  it('should throw an error if reference is zero length',function(done) {
+    (function() {
+      calculator.execute({reference: ''});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+});
+
+describe('alg 06', function() {
+
+  beforeEach(function() {
+    calculator = new CIECalculator();
+    calculator.setStrategy(new Alg06());
+  });
+
+  it('should calculate the check digit', function(done) {
+    calculator.execute({branch: '870', account: '7654321',reference: '87654321'}).should.eql('8765432179');
+    done();
+  });
+
+  it('should throw an error if reference length is longer than 18',function(done) {
+    (function() {
+      calculator.execute({branch: '870', account: '7654321', reference: '1234567890123456789'});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+
+  it('should throw an error if reference is zero length',function(done) {
+    (function() {
+      calculator.execute({branch: '870', account: '7654321', reference: ''});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+
+  it('should throw an error if branch is zero length',function(done) {
+    (function() {
+      calculator.execute({branch: '', account: '7654321', reference: '87654321'});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+  it('should throw an error if account is zero length',function(done) {
+    (function() {
+      calculator.execute({branch: '870', account: '', reference: '87654321'});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+});
+
+describe('alg 10', function() {
+
+  beforeEach(function() {
+    calculator = new CIECalculator();
+    calculator.setStrategy(new Alg10());
+  });
+
+  it('should calculate the check digit', function(done) {
+    calculator.execute({date: '23051997', amount: '4256.50',reference: '067591119700'}).should.eql('06759111970034946226');
+    calculator.execute({date: '18082014', amount: '92832.32',reference: '121341234123'}).should.eql('12134123412399065282');
+    calculator.execute({date: '01011988', amount: '0.01',reference: '1'}).should.eql('100007230');
+    done();
+  });
+
+  it('should throw an error if reference length is longer than 12',function(done) {
+    (function() {
+      calculator.execute({date: '23051997', amount: '4256.50',reference: '0675911197001'});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+
+  it('should throw an error if reference is zero length',function(done) {
+    (function() {
+      calculator.execute({date: '23051997', amount: '4256.50',reference: ''});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+
+  it('should throw an error if amount is zero length',function(done) {
+    (function() {
+      calculator.execute({date: '23051997', amount: '',reference: '067591119700'});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+  it('should throw an error if date is zero length',function(done) {
+    (function() {
+      calculator.execute({date: '', amount: '4256.50',reference: '067591119700'});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+});
+
+describe('alg 35', function() {
+
+  beforeEach(function() {
+    calculator = new CIECalculator();
+    calculator.setStrategy(new Alg35());
+  });
+
+  it('should calculate the check digit', function(done) {
+    calculator.execute({reference: 'ANGELICABA1'}).should.eql('ANGELICABA18');
+    calculator.execute({reference: 'ALGOSHOT1234'}).should.eql('ALGOSHOT12343');
+    calculator.execute({reference: 'PUMBA81'}).should.eql('PUMBA819');
+    calculator.execute({reference: '49CQJA5A0QO8LVU'}).should.eql('49CQJA5A0QO8LVU0');
+    done();
+  });
+
+  it('should accept lower case', function(done) {
+    calculator.execute({reference: '00011772xxya21'}).should.eql('00011772XXYA213');
+    done();
+  });
+
+  it('should throw an error if length is longer than 19',function(done) {
+    (function() {
+      calculator.execute({reference: '12345678901234567890'});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+
+  it('should throw an error if reference length is less than 1',function(done) {
+    (function() {
+      calculator.execute({reference: ''});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+});
+
+describe('alg 36', function() {
+
+  beforeEach(function() {
+    calculator = new CIECalculator();
+    calculator.setStrategy(new Alg36());
+  });
+
+  it('should calculate the check digit', function(done) {
+    calculator.execute({reference: 'AZ00035RG00002L87'}).should.eql('AZ00035RG00002L871');
+    calculator.execute({reference: 'ALGOSHOT1234'}).should.eql('ALGOSHOT12349');
+    calculator.execute({reference: '123400001AXY'}).should.eql('123400001AXY0');
+    calculator.execute({reference: '123400001AXG'}).should.eql('123400001AXG0');
+    done();
+  });
+
+  it('should accept lower case', function(done) {
+    calculator.execute({reference: 'az00035sg00002l87'}).should.eql('AZ00035SG00002L879');
+    done();
+  });
+
+  it('should throw an error if length is longer than 19',function(done) {
+    (function() {
+      calculator.execute({reference: '12345678901234567890'});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+
+  it('should throw an error if reference length is less than 1',function(done) {
+    (function() {
+      calculator.execute({reference: ''});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+});
+
+describe('alg 37', function() {
+
+  beforeEach(function() {
+    calculator = new CIECalculator();
+    calculator.setStrategy(new Alg37());
+  });
+
+  it('should calculate the check digit', function(done) {
+    calculator.execute({reference: '0GFH0500000U000P0'}).should.eql('0GFH0500000U000P014');
+    calculator.execute({reference: '0GFH0500000U000P3'}).should.eql('0GFH0500000U000P314');
+    calculator.execute({reference: 'ALGOSHOT1234'}).should.eql('ALGOSHOT123407');
+    calculator.execute({reference: 'A001BZ12'}).should.eql('A001BZ1273');
+    done();
+  });
+
+  it('should accept lower case', function(done) {
+    calculator.execute({reference: '0gfh0500000u000p0'}).should.eql('0GFH0500000U000P014');
+    done();
+  });
+
+  it('should throw an error if length is longer than 18',function(done) {
+    (function() {
+      calculator.execute({reference: '12345678901234567890'});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+
+  it('should throw an error if reference length is less than 1',function(done) {
+    (function() {
+      calculator.execute({reference: ''});
+    }).should.throw('Longitud invalida');
+    done();
+  });
+});
